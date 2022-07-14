87996f21145d3f8e25b2c20e009cd8568cf58b18
abubakar-hamad <Abubakarhmdelneel@gmail.com>
2022-07-14 11:51:57 +0200

    heroku deploy

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
+import path from 'path';
+import {fileURLToPath} from 'url';
+
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import Post from './routes/PostsRoute.js'
 import User from './routes/UserRoute.js'
 import errorHandler from './middleware/errorMiddleware.js'
-
 
 dotenv.config()
 connectDB()
@@ -13,6 +15,9 @@ import connectDB from './config/db.js'
 
 const port = process.env.PORT || 5000
 const app = express()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 app.use(cors())
 app.use(express.json())
@@ -23,6 +28,15 @@ app.use(errorHandler)
 app.use('/api/post' , Post)
 
 app.use('/api/user' , User)
+
+// server client
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname , '../client/build')))
+    app.get('*' , (req , res) => res.sendFile(path.resolve(__dirname , '../' , 'client' , 'build' , 'index.html')) )
+}else{
+    app.get('/' , (req,res)=> res.send("production"))
+}
 
 
 
